f7830307299463a97feb511bba85dfcb415e7cae
Jumping Boy <jpregbaha@gmail.com>
2021-09-24 10:31:19 +0100

    Adding a new block to blockchain

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -3,11 +3,28 @@ from block import Block
 
 
 class Blockchain:
-    def __init__(self, chain=[], all_transactions=[]):
+    def __init__(self):
+        self.chain = []
+        self.all_transactions = []
         self.genesis_block()
-        self.chain = chain
-        self.all_transactions = all_transactions
 
     def genesis_block(self):
-        new_block = Block([], 0)
+        transactions = {}
+        genesis_block = Block(transactions, "0")
+        self.chain.append(genesis_block)
+        return self.chain
+
+    # prints contents of blockchain
+    def print_blocks(self):
+        for i in range(len(self.chain)):
+            current_block = self.chain[i]
+            print("Block {} {}".format(i, current_block))
+            current_block.print_contents()
+
+    # add block to blockchain `chain`
+    def add_block(self, transactions):
+        previous_block_hash = self.chain[len(self.chain) - 1].hash
+        new_block = Block(transactions, previous_block_hash)
         self.chain.append(new_block)
+        return self.chain
+
