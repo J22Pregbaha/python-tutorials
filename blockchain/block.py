8b7eda6127af4bae46015d861ae705d17a397c18
Jumping Boy <jpregbaha@gmail.com>
2021-09-24 10:03:58 +0100

    Creating the block class

---
+++ blockchain/block.py
@@ -0,0 +1,13 @@
+# import datetime library
+from datetime import datetime
+
+# print current date and time
+print(datetime.now())
+
+class Block:
+# default constructor for block class
+  def __init__(self, transactions, previous_hash, nonce = 0):
+        self.transactions = transactions
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+        self.timestamp = datetime.now()
