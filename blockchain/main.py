708bb8541cb51aca7eb82f5e951c996c7b1f2a23
Jumping Boy <jpregbaha@gmail.com>
2021-09-24 09:52:16 +0100

    Understanding the mempool

---
+++ blockchain/main.py
@@ -0,0 +1,39 @@
+transaction1 = {
+  'amount': '30',
+  'sender': 'Alice',
+  'receiver': 'Bob'
+}
+transaction2 = {
+  'amount': '200',
+  'sender': 'Bob',
+  'receiver': 'Alice'
+}
+transaction3 = {
+  'amount': '300',
+  'sender': 'Alice',
+  'receiver': 'Timothy'
+}
+transaction4 = {
+  'amount': '300',
+  'sender': 'Rodrigo',
+  'receiver': 'Thomas'
+}
+transaction5 = {
+  'amount': '200',
+  'sender': 'Timothy',
+  'receiver': 'Thomas'
+}
+transaction6 = {
+  'amount': '400',
+  'sender': 'Tiffany',
+  'receiver': 'Xavier'
+}
+my_transaction = {
+  'amount': '1000',
+  'sender': 'Joshua',
+  'receiver': 'Daniel'
+}
+
+mempool = [transaction1, transaction2, transaction3, transaction4, transaction5, transaction6, my_transaction]
+
+block_transactions = [transaction1, transaction2, transaction3]
