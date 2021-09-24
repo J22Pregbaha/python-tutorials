0a419e73e7db7b7bd8f23b34fac6453f7203a5f9
Jumping Boy <jpregbaha@gmail.com>
2021-09-24 10:17:20 +0100

    Updating the blocks to create a hash for the current block

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,13 +1,24 @@
-# import datetime library
 from datetime import datetime
+from hashlib import sha256
 
-# print current date and time
-print(datetime.now())
 
 class Block:
-# default constructor for block class
-  def __init__(self, transactions, previous_hash, nonce = 0):
+    def __init__(self, transactions, previous_hash, nonce=0):
+        self.timestamp = datetime.now()
         self.transactions = transactions
         self.previous_hash = previous_hash
         self.nonce = nonce
-        self.timestamp = datetime.now()
+        self.hash = self.generate_hash()
+
+    def print_block(self):
+        # prints block contents
+        print("timestamp:", self.timestamp)
+        print("transactions:", self.transactions)
+        print("current hash:", self.generate_hash())
+
+    def generate_hash(self):
+        block_contents = str(self.timestamp) + str(self.transactions) + str(self.nonce) + str(self.previous_hash)
+
+        # hash the blocks contents
+        block_hash = sha256(block_contents.encode())
+        return block_hash.hexdigest()
