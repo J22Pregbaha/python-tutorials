f52b45524fbe30b30a1cb57bef736e3b02d69679
Jumping Boy <jpregbaha@gmail.com>
2021-09-24 11:32:38 +0100

    Script to check proof of work

---
+++ blockchain/proof_of_work.py
@@ -0,0 +1,30 @@
+new_transactions = [{'amount': '30', 'sender': 'alice', 'receiver': 'bob'},
+                    {'amount': '55', 'sender': 'bob', 'receiver': 'alice'}]
+
+# import sha256
+from hashlib import sha256
+
+# sets the amount of leading zeros (difficulty) that must be found in the hash produced by the nonce
+difficulty = 2
+nonce = 0
+
+# creating the proof
+transaction = str(new_transactions) + str(nonce)
+proof = sha256(transaction.encode()).hexdigest()
+
+# printing proof
+print(proof)
+
+# finding a proof that has 2 leading zeros
+not_yet = True
+while not_yet:
+    transaction = str(new_transactions) + str(nonce)
+    proof = sha256(transaction.encode()).hexdigest()
+    nonce = nonce + 1
+    if proof[0:difficulty].isnumeric():
+        if int(proof[0:difficulty]) == 0:
+            final_proof = proof
+            not_yet = False
+
+# printing final proof that was found
+print(final_proof)
