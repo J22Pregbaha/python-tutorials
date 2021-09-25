2ab4cbf280710899cab4f90136340fe221ad87bd
Jumping Boy <jpregbaha@gmail.com>
2021-09-25 08:59:19 +0100

    Experimented with nodes

--- linear-data-structures/nodes.py
+++ linear-data-structures/nodes.py
@@ -11,3 +11,17 @@ class Node:
 
   def set_link_node(self, link_node):
     self.link_node = link_node
+
+yacko = Node("likes to yak")
+wacko = Node("has a penchant for hoarding snacks")
+dot = Node("enjoys spending time in movie lots")
+
+# yacko => dot => wacko
+yacko.set_link_node(dot)
+dot.set_link_node(wacko)
+# get dot‘s value from yacko
+dots_data = yacko.get_link_node().get_value()
+print(dots_data)
+# get wacko‘s value from dot
+wackos_data = dot.get_link_node().get_value()
+print(wackos_data)
