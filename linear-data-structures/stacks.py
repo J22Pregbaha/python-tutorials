0515244bc7bf50215f7cbe038eedd5eb77b49401
Jumping Boy <jpregbaha@gmail.com>
2021-09-27 20:35:05 +0100

    Add data to test stack

--- linear-data-structures/stacks.py
+++ linear-data-structures/stacks.py
@@ -13,26 +13,54 @@ class Stack:
             item.set_next_node(self.top_item)
             self.top_item = item
             self.size += 1
+            print("Adding {} to the pizza stack!".format(value))
         else:
-            return "All out of space!"
+            print("No room for {}!".format(value))
 
     def pop(self):
-        if self.is_empty():
+        if not self.is_empty():
             item_to_remove = self.top_item
             self.top_item = item_to_remove.get_next_node()
             self.size -= 1
+            print("Delivering " + item_to_remove.get_value())
             return item_to_remove.get_value()
-        else:
-            print("This stack is totally empty.")
+
+        print("All out of pizza.")
 
     def peek(self):
-        if self.is_empty():
+        if not self.is_empty():
             return self.top_item.get_value()
-        else:
-            print("Nothing to see here!")
+
+        return "Nothing to see here!"
 
     def has_space(self):
         return self.limit > self.size
 
     def is_empty(self):
         return self.size == 0
+
+
+# Defining an empty pizza stack with a limit of 6
+pizza_stack = Stack(6)
+# Adding pizzas as they are ready until we have
+pizza_stack.push("pizza #1")
+pizza_stack.push("pizza #2")
+pizza_stack.push("pizza #3")
+pizza_stack.push("pizza #4")
+pizza_stack.push("pizza #5")
+pizza_stack.push("pizza #6")
+
+# Try to add a pizza that's over the size
+pizza_stack.push("pizza #7")
+
+# Delivering pizzas from the top of the stack down
+print("The first pizza to deliver is " + pizza_stack.peek())
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+
+# Try to remove pizza from empty stack
+pizza_stack.pop()
