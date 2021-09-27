6a2be3dbb28fef5aecd334bcb7afdd13a211725f
Jumping Boy <jpregbaha@gmail.com>
2021-09-27 19:57:04 +0100

    Added checks to prevent stack overflow

--- linear-data-structures/stacks.py
+++ linear-data-structures/stacks.py
@@ -2,8 +2,10 @@ from linked_list import Node
 
 
 class Stack:
-    def __init__(self):
+    def __init__(self, limit=1000):
         self.top_item = None
+        self.size = 0
+        self.limit = limit
 
     def push(self, value):
         item = Node(value)
@@ -11,9 +13,16 @@ class Stack:
         self.top_item = item
 
     def pop(self):
-        item_to_remove = self.top_item
-        self.top_item = item_to_remove.get_next_node()
-        return item_to_remove.get_value()
+        if self.size > 0:
+            item_to_remove = self.top_item
+            self.top_item = item_to_remove.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
+        else:
+            return "Stack is empty"
 
     def peek(self):
-        return self.top_item.get_value()
+        if self.size > 0:
+            return self.top_item.get_value()
+        else:
+            return "Stack is empty"
