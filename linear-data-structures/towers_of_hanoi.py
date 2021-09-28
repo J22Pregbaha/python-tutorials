a6aa195975d2f5bb60ec83f757a416f9e5ddb2c0
Jumping Boy <jpregbaha@gmail.com>
2021-09-28 12:42:05 +0100

    Setting up the game

--- linear-data-structures/towers_of_hanoi.py
+++ linear-data-structures/towers_of_hanoi.py
@@ -12,7 +12,16 @@ stacks.append(middle_stack)
 stacks.append(right_stack)
 
 # Set up the Game
+num_disks = int(input("\nHow many disks do you want to play with?\n"))
+# The game is fun with at least 3 discs
+while num_disks < 3:
+    num_disks = int(input("Enter a number greater than or equal to 3\n"))
 
+for i in range(num_disks, 0, -1):
+    left_stack.push(i)
+
+num_optimal_moves = (2 ** num_disks) - 1
+print("\nThe fastest you can solve this game is in {0} moves".format(num_optimal_moves))
 
 # Get User Input
 
