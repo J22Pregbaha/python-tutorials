101f7a836657a2e52aee9f115895c1cce95e2e47
Jumping Boy <jpregbaha@gmail.com>
2021-09-28 14:49:06 +0100

    More tests

--- linear-data-structures/queue.py
+++ linear-data-structures/queue.py
@@ -60,7 +60,36 @@ class Queue:
     def is_empty(self):
         return self.get_size() == 0
 
-q = Queue(1)
-q.enqueue("some guy with a mustache")
-q.enqueue("some guy with a mustachio")
-q.dequeue()
+print("Creating a deli line with up to 10 orders...\n------------")
+deli_line = Queue(10)
+print("Adding orders to our deli line...\n------------")
+deli_line.enqueue("egg and cheese on a roll")
+deli_line.enqueue("bacon, egg, and cheese on a roll")
+deli_line.enqueue("toasted sesame bagel with butter and jelly")
+deli_line.enqueue("toasted roll with butter")
+deli_line.enqueue("bacon, egg, and cheese on a plain bagel")
+deli_line.enqueue("two fried eggs with home fries and ketchup")
+deli_line.enqueue("egg and cheese on a roll with jalapeos")
+deli_line.enqueue("plain bagel with plain cream cheese")
+deli_line.enqueue("blueberry muffin toasted with butter")
+deli_line.enqueue("bacon, egg, and cheese on a roll")
+# ------------------------ #
+# Overflow
+deli_line.enqueue("western omelet with home fries")
+# ------------------------ #
+print("------------\nOur first order will be " + deli_line.peek())
+print("------------\nNow serving...\n------------")
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+# ------------------------ #
+# Underflow:
+deli_line.dequeue()
+# ------------------------ #
